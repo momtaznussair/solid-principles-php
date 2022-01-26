916cea19cac89ddc319331c83e6220ccec343509
momtaz <momtaz_nussair@yahoo.com>
2022-01-26 09:14:20 +0200

    Create MatchControl Class With Violation to SRP => MatchControl is Responsible for Three jobs attackm defence and keeping

---
+++ src/Classes/Match.php
@@ -0,0 +1,15 @@
+<?php
+
+class MatchControl {
+    public function attack() {
+        # code...
+    }
+
+    public function defence() {
+        # code...
+    }
+
+    public function keeping() {
+        # code...
+    }
+}
